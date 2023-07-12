e5f3cd952c4e7de30712c2741d2f9ab19ae967e1
Will Bowman <wpb.astro@gmail.com>
2023-07-12 09:54:27 -0500

    edit init

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
+__version__ = "0.0.1"
+
 from . import imageqa
-from imageqa import *
+#from imageqa import *
 
 # not sure yet how the following line should be organized, since it is nested
-from .detection import *
+#from .detection import *
