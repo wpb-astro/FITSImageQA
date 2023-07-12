dc53b513bb3d652432d1f2a2e7e16d90d8ff2d7b
Will Bowman <wpb.astro@gmail.com>
2023-07-12 09:31:37 -0500

    package reorg + adding default files

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+    name = "FITSImageQA", 
+    description = "Perform quality checks on FITS images",
+    long_description = long_description, 
+    long_description_content_type = "text/markdown",
+    version = "0.0.1",  # TODO: how to reconcile version set here vs in the `__init__.py` file
+    license = "MIT",  # TODO: confirm correct 
+    #author = 
+    #author_email = 
+    url = "https://github.com/wpb-astro/FITSImageQA",
+    include_package_data = True, # looks in MANIFEST.in
+    packages = find_packages(where="src"),
+    #package_data = 
+    #package_dir = 
+    #install_requires = 
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy",
+      ],
+    python_requires='>=3.7', 
+    #keywords = 
+    #entry_points = 
+
+
+
+
+
+
+
+)
