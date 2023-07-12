28d689a3713017ea5d7cf713410b76cffcfdf8c9
Will Bowman <wpb.astro@gmail.com>
2023-07-12 11:31:09 -0500

    improve docstrings

--- src/FITSImageQA/imageqa.py
+++ src/FITSImageQA/imageqa.py
@@ -9,9 +9,9 @@ date XYZ
 from __future__ import annotations
 
 # imports:
+import numpy as np
 from astropy.io import fits
 import logging
-import sep
 
 # package imports
 # TODO: not sure how these should be organized...
@@ -47,7 +47,10 @@ class Thing:
         pass
 
 class QAHeader(Thing):
-    def __init__(self, filename_or_hdr, expected_fields=None, expected_fields_dtype=None) -> None:
+    def __init__(self, filename_or_hdr: str | fits.hdu.hdulist.HDUList | fits.header.Header, 
+                 expected_fields: list[str] = None, 
+                 expected_fields_dtype: dict = None
+                ) -> None:
         """
 
         Parameters
@@ -57,7 +60,7 @@ class QAHeader(Thing):
             can be a filename, or an hdu (result of fits.open), or fitsheader object (hdu[0].header)
         expected_fields : iterable of str (optional)
             list of fields that must appear in the image header
-        expected_fields_dtype : iter of key-value pairs (optional)
+        expected_fields_dtype : dict of key-value pairs (optional)
             keys = header names (str)
             values = data types        
         """
@@ -79,7 +82,7 @@ class QAHeader(Thing):
             self.expected_fields = expected_fields
         self.expected_fields_dtype = expected_fields_dtype
 
-    def fetch_header_info(self, column_name):
+    def fetch_header_info(self, column_name: str):
         """ 
         Get info from the header
 
@@ -109,9 +112,9 @@ class QAHeader(Thing):
         #except KeyError:
         #    self.logger.error("Field not found in the header.")
 
-    def check_header_fields_present(self, expected_fields=None, 
-                                    return_missing_fields=False,
-                                    overwrite_attribute=False):
+    def check_header_fields_present(self, expected_fields: list[str] = None, 
+                                    return_missing_fields: bool = False,
+                                    overwrite_attribute: bool = False):
         """
 
         Parameters
@@ -146,9 +149,9 @@ class QAHeader(Thing):
             missing_fields = expected_fields.difference( self.header_fields )
             return valid, missing_fields
     
-    def check_header_fields_dtype(self, expected_fields_dtype=None,
-                                  return_incorrect_fields=False,
-                                  suppress_unknown=False, verbose=False):
+    def check_header_fields_dtype(self, expected_fields_dtype: dict = None,
+                                  return_incorrect_fields: bool = False,
+                                  suppress_unknown: bool = False, verbose: bool = False):
         """
         Check that data type of header fields are as expected
         
@@ -173,7 +176,9 @@ class QAHeader(Thing):
         pass
 
 class QAData(Thing):
-    def __init__(self, filename_or_data, detection_config=None) -> None:
+    def __init__(self, filename_or_data: str | fits.hdu.hdulist.HDUList | np.ndarray, 
+                 detection_config: dict = None
+                ) -> None:
         """
 
         Parameters
@@ -190,12 +195,14 @@ class QAData(Thing):
             data = fits.getdata(filename_or_data)
         elif isinstance(filename_or_data, fits.hdu.hdulist.HDUList):
             data = filename_or_data[0].data
-        else:
+        elif isinstance(filename_or_data, np.ndarray)
             data = filename_or_data
+        else:
+            raise TypeError("filename_or_data is not the correct type.")
         self.data = data
         self.detection_config = detection_config
 
-    def is_focus_good(self, max_focus_fwhm=2.5): #or, rename as check_focus
+    def is_focus_good(self, max_focus_fwhm: float | int = 2.5): 
         """
         Determine whether a focus run is needed while observing,
         by comparing the FWHM of sources that are detected in the image
@@ -228,7 +235,8 @@ class QAData(Thing):
         in_focus = med_fwhm <= max_focus_fwhm
         return in_focus, med_fwhm
 
-    def detect_sources(self, detection_config=None, overwrite=True, **kwargs):
+    def detect_sources(self, detection_config: dict = None, 
+                       overwrite: bool = True, **kwargs):
         """ 
         Detect sources in the image and store the result as an attribute
 
